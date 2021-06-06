b6ac1d11bff9d0702420208f2eeb4f956c46bbac
Hrosc <us6796@student.uni-lj.si>
2021-06-06 19:12:41 +0200

    pipeline

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 import os
 import pickle
+from pgn_parser import *
+from embeddings import *
 from classifier import *
 
+games, results = parser("data/lichess/lichess_db_standard_rated_2017-02.pgn")
+G, all_results = create_metaposition_network(games, results)
+embeddings = metaposition_node2vec(G, all_results)
 
+# with open(os.path.join("output", "embeddings"), 'rb') as file:
+#     embeddings = pickle.load(file)
 
-with open(os.path.join("output", "embeddings"), 'rb') as file:
-    data = pickle.load(file)
-X, y = parse_embeddings("embeddings")
+X, y = parse_embeddings(embeddings)
 
 clf = logistic_regression()
 clf.train(X, y)
